346dd69075f2262f8f07ecd20f5303309b924611
Yuta0120 <yutahamaguchi@web.de>
2016-07-24 20:05:59 +0900

    how many times should i ....

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
- root to: "top#index"
+ root "top#index"
  resources :users, only: [:index, :show, :new, :create, :destroy, :edit, :update]
  resources :mantos, only: [:index, :show, :new, :create, :destroy, :edit, :update]
  resource :top
